5066596b68ab3f80e35f266718c874bdedc32b22
NirojXhettri <120495988+NirojXhettri@users.noreply.github.com>
2024-04-16 21:37:05 +0545

    added index.html added style.css
    added script.js added Ichhya.jpg added sound.mp3

---
+++ script.js
@@ -0,0 +1,30 @@
+
+setInterval(setClock, 1000);
+
+const hourHands = document.querySelector("[data-hour-hand]");
+const minHands = document.querySelector("[data-min-hand]");
+const secHands = document.querySelector("[data-sec-hand]");
+const tickTockSound = document.getElementById("tick-tock");
+
+function playTickTock() {
+  tickTockSound.play();
+}
+
+setInterval(playTickTock, 1000);
+
+
+function setClock() {
+  const currentDate = new Date();
+  const secRatio = currentDate.getSeconds() / 60;
+  const minRatio = currentDate.getMinutes() / 60;
+  const hourRatio = (currentDate.getHours() + minRatio) / 12;
+  setRotation(secHands, secRatio);
+  setRotation(minHands, minRatio);
+  setRotation(hourHands, hourRatio);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty("--rotation", rotationRatio * 360);
+}
+
+setClock();
